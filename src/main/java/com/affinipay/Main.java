d5cf07146f3a39630e058f1a683a0858026a44e3
Ian Bush <ianfracnisbush@gmail.com>
2020-11-08 17:50:13 -0500

    removing call used for intial testing from main

--- src/main/java/com/affinipay/Main.java
+++ src/main/java/com/affinipay/Main.java
@@ -3,11 +3,8 @@ package com.affinipay;
 
 public class Main {
     public static void main(String[] args) {
+        
 
-        CalculateTime calculateTime = new CalculateTime();
-//1400
-        System.out.println(calculateTime.addMinutes("9:10 AM", 720));
-
-    }
+        }
 
     }
